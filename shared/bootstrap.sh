5a9c6052566b7d8c38563863a251fc58aa67050c
henry.cook <henry.cook@century.tech>
2017-04-13 18:09:13 +0100

    Disable selinux for DCOS installer

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 sudo yum -y 
-
 sudo yum install -y docker unzip
 
 sudo /bin/systemctl start docker.service
 
-sudo mkdir -p /root/.ssh/
+sudo setenforce Permissive
+sudo echo 'SELINUX=disabled' >> /etc/sysconfig/selinux
 
+sudo mkdir -p /root/.ssh/
 sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
 
 exit 0
