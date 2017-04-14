b179fbdb64cb94f5b3aa23b9453fe11a7786b635
Henry Cook <henry.cook@outlook.com>
2017-04-14 10:58:27 +0100

    Copying ssh keys if they don't exist and some docker config ammendments for DCOS installer

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -4,20 +4,19 @@
 sudo yum -y 
 sudo yum install -y docker unzip
 
-
-for i in {0..6}
-do
-    mknod -m0660 /dev/loop$i b 7 $i
-done
-
-sudo /bin/systemctl start docker.service
+# Configuring and restarting Docker service
+echo 'DOCKER_OPTS="--storage-driver=overlay"' >> /etc/default/docker
+sudo /bin/systemctl restart docker.service
 
 # Disable selinux
 sudo setenforce Permissive
 sudo echo 'SELINUX=disabled' >> /etc/sysconfig/selinux
 
 # Copy SSH keys to nodes
-sudo mkdir -p /root/.ssh/
-sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
+if [ ! -f dcos_generate_config.sh ]; then
+    echo "id_rsa.pub does not exist, copying over now."
+    sudo mkdir -p /root/.ssh/
+    sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
+fi
 
 exit 0
