9bb3dde1ae6f1f8c5752c593c69891ee7ed09881
Henry Cook <henry.cook@outlook.com>
2017-04-14 11:49:29 +0100

    Removing pointless complexity

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -32,10 +32,7 @@ sudo setenforce Permissive
 sudo echo 'SELINUX=disabled' >> /etc/sysconfig/selinux
 
 # Copy SSH keys to nodes
-if [ ! -f dcos_generate_config.sh ]; then
-    echo "id_rsa.pub does not exist, copying over now."
-    sudo mkdir -p /root/.ssh/
-    sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
-fi
+sudo mkdir -p /root/.ssh/
+sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
 
 exit 0
