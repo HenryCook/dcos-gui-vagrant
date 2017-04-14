4563367d1fc4f9cdb67ac144206967c5f70beb24
Henry Cook <henry.cook@outlook.com>
2017-04-14 10:49:48 +0100

    Comments for bootstrap.sh

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Install Dependencies
 sudo yum -y 
 sudo yum install -y docker unzip
 
+
+for i in {0..6}
+do
+    mknod -m0660 /dev/loop$i b 7 $i
+done
+
 sudo /bin/systemctl start docker.service
 
+# Disable selinux
 sudo setenforce Permissive
 sudo echo 'SELINUX=disabled' >> /etc/sysconfig/selinux
 
+# Copy SSH keys to nodes
 sudo mkdir -p /root/.ssh/
 sudo cat /vagrant_shared/id_rsa.pub >> /root/.ssh/authorized_keys
 
